aa1260ab800c105eca696ba1bb70d010eb7c779b
456Frodo <marcusensley074@gmail.com>
2022-10-18 11:55:10 -0500

    doubled input parsing

--- prj2.asm
+++ prj2.asm
@@ -1,21 +1,40 @@
+#Group 7
 #Marcus Ensley 1574206 456Frodo
 #Rhea Katari 
 #Kaitlin Colvin
 
 #add data for error return statement
+.data 
+errorString: .asciiz "input error!\n"
 
+.text 
+.globl main
+
+main:
 #do a second syscall for getting multiplier
-#parse string from console
-addi $a0, $gp, 4 #set string 1 away from gp
-addi $a1, $zero, 5 #set string length to 3 #NEEDS TO BE CAHANGED
+
+#parse string 1 (multicand) from console
+addi $a0, $gp, 4 #set string 1 byte away from gp
+addi $a1, $zero, 15 #set string length to 15 
 addi $v0, $zero, 8 # 8 syscall code for reading string
 syscall
 
+#string 1 is stored 1 byte away from gp
+
+#parse string 2 (multiplier) from console
+addi $a0, $gp, 72 #set string  away 18 bytes from gp
+addi $a1, $zero, 15 #set string length to 15 
+addi $v0, $zero, 8 # 8 syscall code for reading string
+syscall
+
+#string 2 is stored 18 bytes from gp
 
 #add single check for negative sign
 #add checkers for non numerical ascii values, break to input error return
 
 #change string to int and put in a0
+
+#transfer first string into int in register a0
 addi $t0, $gp, 4 #set t0 to point to string
 addi $s0, $zero, 10 #set s0 to ten for mult 
 add $a0, $zero, $zero
