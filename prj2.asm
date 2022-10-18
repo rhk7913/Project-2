daefc7b485edb60439b5b99f315a6ab59ce98e02
456Frodo <marcusensley074@gmail.com>
2022-10-18 12:42:07 -0500

    Update prj2.asm

--- prj2.asm
+++ prj2.asm
@@ -12,6 +12,9 @@ errorString: .asciiz "input error!\n"
 
 main:
 #do a second syscall for getting multiplier
+#protect ra in stack 
+addi $sp, $sp, -4
+sw $ra, 0($sp) 
 
 #parse string 1 (multicand) from console
 addi $a0, $gp, 4 #set string 1 byte away from gp
@@ -32,16 +35,24 @@ syscall
 #add single check for negative sign
 #add checkers for non numerical ascii values, break to input error return
 
-#change string to int and put in a0
+#transfer first string into int and put in register a0
 
-#transfer first string into int in register a0
 addi $t0, $gp, 4 #set t0 to point to string
 addi $s0, $zero, 10 #set s0 to ten for mult 
 add $a0, $zero, $zero
+lbu $t1, 0($t0) #load first byte in t1
+beq $t1, 45, negative #if first byte is equal to - jump to negative
+j checked #if not jump into second line of loop
+
 load:
 lbu $t1, 0($t0) #load byte from string in t1
+checked:
 beq $t1, 10, stringDone 
 mult $a0, $s0 #a0 times 10
+#check to see if high is greater than zero
+mfhi $t3 #move high register into t3
+slt $t4, $zero, $t3 
+beq $t4, $zero, errorReturn #jump to errorReturn if there is overflow in register
 mflo $a0 #move product into a0 #check lows and hughs
 addi $t2, $t1, -48 #t2 decimal value of number
 add $a0, $a0, $t2 #add value to a0
@@ -50,17 +61,16 @@ j load
 
 #check output for overflow (hint use high low)
 
+#set s7 to 1 if a number is negative
+checked:
+addi $s7, $zero, 1 #set boolean s7 to one
+j checked #jump to second line of loop
+
 stringDone:
-#call inputString
-add $t0, $a0, $zero #protect a0 in t0
-la $a0, inputString #store string in a0
-addi $v0, $zero, 4 #syscall for print string
-syscall
-add $a0, $t0, $zero #put t0 in a0
-addi $v0, $zero, 1
-syscall 
-la $a0, endString #store endString in a0
-addi $v0, $zero, 4 #syscall for print string
-syscall
-add $a0, $t0, $zero #put t0 in a0
+#check to see if either string is negative
+#if so change binary to be twos complement 
+#print out the binary of the 32 bit multicand and multiplier
+
+#if error in input return errorString
+errorReturn: 
 
